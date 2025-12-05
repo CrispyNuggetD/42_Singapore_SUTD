b71a7f70062f0b24b1af9da33181badc7a98703d
hnah 42SG <hnah@student.42singapore.sg>
2025-12-05 10:31:52 +0800

    Stdheader for libft.h "header" (lol)

--- Core Curriculum/Projects/1_libft/libft.h
+++ Core Curriculum/Projects/1_libft/libft.h
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnah <hnah@student.42singapore.sg>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/05 10:30:18 by hnah              #+#    #+#             */
+/*   Updated: 2025/12/05 10:31:04 by hnah             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <stdlib.h>
 # include <unistd.h>
 
-int	ft_isalpha(int c);
+int		ft_isalpha(int c);
 /* FILL IN MISSING FUNCTIONS HERE */
 char	*ft_itoa(int n);
 
