0835b9c64db21393d09cc819a2586d30dec06020
hnah 42SG <hnah@student.42singapore.sg>
2025-12-05 09:58:41 +0800

    Failed 24h challenge; syncing
    apparently is 0000-2359

--- Core Curriculum/Projects/1_libft/libft.h
+++ Core Curriculum/Projects/1_libft/libft.h
@@ -1,8 +1,10 @@
 #ifndef LIBFT_H
-#   define LIBFT_H
+# define LIBFT_H
+# include <stdlib.h>
+# include <unistd.h>
 
-int ft_isalpha(int c);
+int	ft_isalpha(int c);
 /* FILL IN MISSING FUNCTIONS HERE */
-char *ft_itoa(int n);
+char	*ft_itoa(int n);
 
 #endif
