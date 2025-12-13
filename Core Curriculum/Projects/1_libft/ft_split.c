90862bdc313754219a3f459959b096184e25621c
hnah 42SG <hnah@student.42singapore.sg>
2025-12-13 19:33:55 +0800

    EOD Dec 13 ft_split pain.
    No longer seg fault but copy logic weird

--- Core Curriculum/Projects/1_libft/ft_split.c
+++ Core Curriculum/Projects/1_libft/ft_split.c
@@ -1,10 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnah <hnah@student.42singapore.sg>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/13 16:30:36 by hnah              #+#    #+#             */
+/*   Updated: 2025/12/13 18:18:40 by hnah             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	copied;
+	size_t	len_src;
+
+	len_src = 0;
+	copied = 0;
+	while (src[len_src])
+		len_src++;
+	if (size == 0)
+		return (len_src);
+	while (src[copied] && (copied + 1 < size))
+	{
+		dst[copied] = src[copied];
+		copied++;
+	}
+	dst[copied] = '\0';
+	return (len_src);
+}
 
 char	**ft_split(char const *s, char c)
 {
 	char	**result;
-	char	*temp;
-	size_t	words
+	size_t	words;
 	size_t	len;
 	size_t	end;
 	
@@ -12,42 +43,60 @@ char	**ft_split(char const *s, char c)
 	len = 0;
 	if (!s)
 		return (NULL);
+	while (s[len] == c)
+		len++;
 	while (s[len])
 	{
-		if (s[len] == c && len != 0 && s[len] != s [len - 1])
-			words += 1;
-		len++;
+		while (s[len] && s[len] != c)
+			len++;
+		words++;
+		while (s[len] && s[len] == c)
+        	len++;
 	}
 	result = malloc(sizeof(char *) * (words + 1));
 	if (!result)
 		return (NULL);
 	result[words] = NULL;
+	while (s[len] == c || s[len] == '\0')
+		len--;
 	while (words > 0)
 	{
-		while (s[len] == c && s[len] == '\0')
+		end = len + 1;
+		while (s[len - 1] != c && (len > 0))
 			len--;
-		end = len;
-		while (s[len] != c && len > 0）
-			len--;
-		temp = malloc(sizeof(char) * (end - len + 2));
-		if (!temp)
+		result[words - 1] = malloc(sizeof(char) * (end - len + 1));
+		if (!result[words - 1])
 		{
-			while (words >= 0)
+			while (words != 0)
 				free(result[words--]);
 			free(result);
 			return (NULL);
 		}
-		strlcpy(result[words - 1], s[len], end - len + 2);
+		ft_strlcpy(result[words - 1], &s[len], end - len + 1);
+		while (s[len] == c && (len > 0))
+			len--;
+		len++;
 		words--;
+	}printf("%s",result[words]);
+	return (result);
+}
+
+int	main(void)
+{
+	char	**test;
+	size_t	i=0;
+
+	test = ft_split("  test wdq qwd   ", ' ');
+	while (test[i]){
+		printf("%zu | %s,\n\n", i,test[i++]);
 	}
 }
 
-// 0:A		1:B		2:c		3	4	5
-// len:0	end:1	3:c		3	4	5
+// 0		1		2:c		3	4	5		\0
+// 0		1		2:start	3	4	end:5	\0
 // 
-// Str. length = (1-0) + 1
+// Str. length = (5-2) = 3
 // Needed for null terminator = 1
-// Buffer w. null = 1 + 1 = 2
-// Hence, total to malloc/ strlcpy = (1-0) + 2
+// Hence, total to malloc/ strlcpy = (5-2) + 1
 //
 // Started this w/o. Stdheader (to update) at 1:37pm 13 Dec Sat 2025
