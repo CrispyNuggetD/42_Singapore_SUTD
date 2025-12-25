1ec20dcb5391d77de70302c671ef496fc20f05ad
hnah 42SG <hnah@student.42singapore.sg>
2025-12-25 17:24:22 +0800

    Evening break Christmas'25 lots of admin
    - Merry Christmas! I'm bored, so why not study something I love?

---
+++ Core Curriculum/Projects/1_ft_printf/libft/ft_strmapi.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnah <hnah@student.42singapore.sg>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/12 18:12:14 by hnah              #+#    #+#             */
+/*   Updated: 2025/12/17 16:37:29 by hnah             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*result;
+	size_t	i;
+	size_t	len;
+
+	if (!s || !f)
+		return (NULL);
+	len = ft_strlen(s);
+	result = malloc(sizeof(char) * (len + 1));
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		result[i] = f((unsigned int)i, s[i]);
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
