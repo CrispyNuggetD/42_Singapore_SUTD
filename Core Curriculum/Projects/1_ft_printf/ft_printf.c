fcb2b29e97387e4d7765a19c59d52027a62b49ee
hnah 42SG <hnah@student.42singapore.sg>
2026-01-01 19:05:53 +0800

    EOD Jan 1 Thurs

--- Core Curriculum/Projects/1_ft_printf/ft_printf.c
+++ Core Curriculum/Projects/1_ft_printf/ft_printf.c
@@ -31,15 +31,17 @@ int	main(void)
 // This function is summoned from main to parse the input
 int ft_printf(const char *str, ...)
 {
-	int		printed;
-	int		parsing_passed;
-	va_list	input;
-	t_spec	spec;
+	int			printed;
+	int			parsing_passed;
+	int			current_len;
+	t_context	context;
+	t_spec		spec;
+	va_list		input;
 
 	printed = 0;
-	va_start(input, str);
 	if (!str)
-		return (0);
+		return (0); // check actual error code
+	va_start(input, str);
 	while (*str)
 	{
 		if (*str != '%')
@@ -52,7 +54,23 @@ int ft_printf(const char *str, ...)
 		{
 			str++;
 			parsing_passed = parse_spec(&spec, &str);
-			printed += dispatch_key(&spec, &input);
+			if (parsing_passed)
+			{
+				context.input = &input;
+				context.spec = &spec;
+				current_len = dispatch_key(&context);
+				if (current_len < 0)
+				{
+					va_end(input);
+					return (-1);
+				}
+				printed += current_len;
+			}
+			else
+			{
+				va_end(input);
+				return (-1);
+			}
 		}
 	}
 	va_end(input);
@@ -61,23 +79,25 @@ int ft_printf(const char *str, ...)
 
 // The below function initialises (by mapping) the function handlers
 // Variables mean type_handler, global_handler
-static int	dispatch_key(t_spec *spec, va_list *input)
+static int	dispatch_key(t_context *context)
 {
 	t_handler	fn;
 	int			len_printed;
 
-	fn = init_get_handlers(spec -> conversion);
+	fn = init_get_handlers(context -> spec -> conversion);
 	if (fn)
-		len_printed = fn(spec, input);
+		len_printed = fn(context);
+	else
+		return (-1);
 	return (len_printed);
 }
 
 // Initialise keys and also allows dispatcher to fetch those keys
-static t_handler	init_get_handlers(unsigned char fn_keys)
+static t_handler	init_get_handlers(unsigned char fn_key)
 {
 	static t_handler const	handlers[256] = {
 		['i'] = print_d_i,
 		['d'] = print_d_i,
 	};
-	return (handlers[fn_keys]);
+	return (handlers[fn_key]);
 }
