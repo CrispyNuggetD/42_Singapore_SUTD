83cf648da77c28b07321cb0aefb127bb88218161
Christopher Hui-Kang Nah <christopher_nah@yahoo.com.sg>
2026-01-01 22:39:34 +0800

    added comment compiler error in printf statement

--- Core Curriculum/Projects/1_ft_printf/printf_testing.c
+++ Core Curriculum/Projects/1_ft_printf/printf_testing.c
@@ -24,7 +24,7 @@ int	main(void)
 {
 	int test = (0 == -0); // passing -0 is same as passing 0 (NULL)
 	
-	printf("%i\n",,);
+	printf("%i\n",,); // compiler error
 	printf("%i\n",test);  // NORMAL print
 	printf("%s","huh\n");  // NORMAL print
 	//printf(%s);  // Does not allow/ compile fail
