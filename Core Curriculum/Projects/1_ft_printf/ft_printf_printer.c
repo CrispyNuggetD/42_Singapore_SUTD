5df63d2ada34db439fe26f1f4b616555e4d0b2ac
hnah 42SG <hnah@student.42singapore.sg>
2026-01-03 07:29:16 +0800

    Readme of, and a lot of printf(SOD)

--- Core Curriculum/Projects/1_ft_printf/ft_printf_printer.c
+++ Core Curriculum/Projects/1_ft_printf/ft_printf_printer.c
@@ -1,20 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf_helpers.c                                :+:      :+:    :+:   */
+/*   ft_printf_printer.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hnah <hnah@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/02 12:17:39 by hnah              #+#    #+#             */
-/*   Updated: 2026/01/03 02:17:05 by hnah             ###   ########.fr       */
+/*   Created: 2026/01/03 02:14:55 by hnah              #+#    #+#             */
+/*   Updated: 2026/01/03 02:17:11 by hnah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-// Close va_list and return error
-int	ft_printf_error_end_stream(va_list *input)
+int	ft_printf_printer(t_context *context)
 {
-	va_end(*input);
-	return (-1);
+	ssize_t	print_success;
+	
+	(void)context;
+	print_success = write(1, "%", 1);
+	if (print_success < 0)
+		return (-1);
+	return (1);
 }
