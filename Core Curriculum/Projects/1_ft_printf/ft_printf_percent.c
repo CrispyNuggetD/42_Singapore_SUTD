7205d5b26ee20c0ee93f21d4255ede9a97e2fb64
hnah 42SG <hnah@student.42singapore.sg>
2026-01-07 19:44:07 +0800

    EOD evening 7 jan printf b4 24h

--- Core Curriculum/Projects/1_ft_printf/ft_printf_percent.c
+++ Core Curriculum/Projects/1_ft_printf/ft_printf_percent.c
@@ -6,7 +6,7 @@
 /*   By: hnah <hnah@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/26 09:34:40 by hnah              #+#    #+#             */
-/*   Updated: 2026/01/07 13:17:41 by hnah             ###   ########.fr       */
+/*   Updated: 2026/01/07 19:36:27 by hnah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,10 @@
 int	ft_printf_percent(t_context *context)
 {
 	t_print	paper;
-	int		printer_len;
 
 	(void)context;
 	ft_printf_init_t_print(&paper);
 	paper.core_len = 1;
 	paper.core = "%";
-	printer_len = ft_printf_print_config(context, &paper);
-	return (printer_len);
+	return (ft_printf_print_config(context, &paper));
 }
--- Core Curriculum/Projects/1_ft_printf/ft_printf_unsigned_int.c
+++ Core Curriculum/Projects/1_ft_printf/ft_printf_unsigned_int.c
@@ -6,13 +6,13 @@
 /*   By: hnah <hnah@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/26 09:34:40 by hnah              #+#    #+#             */
-/*   Updated: 2026/01/07 14:24:21 by hnah             ###   ########.fr       */
+/*   Updated: 2026/01/07 19:41:22 by hnah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static size_t	itoa_no_sign(int n, char buf[12], const char **start);
+static size_t	itoa_no_sign (unsigned int n, char buf[12], const char **start);
 static void		print_sign(const t_context *context, t_print *paper, int arg);
 
 int	ft_printf_unsigned_int(t_context *context)
@@ -20,16 +20,22 @@ int	ft_printf_unsigned_int(t_context *context)
 	char		text[12];
 	const char	*start;
 	int			arg;
-	int			printer_len;
+	size_t		digit_len;
 	t_print		paper;
 
 	ft_printf_init_t_print(&paper);
-	arg = va_arg(*(context -> input), int);
+	arg = va_arg(*(context -> input), unsigned int);
 	print_sign(context, &paper, arg);
-	paper.core_len = itoa_no_sign(arg, text, &start);
+	digit_len = itoa_no_sign(arg, text, &start);
+	if ((context -> spec -> flags & FLAG_PREC) && \
+context -> spec -> precision == 0 && arg == 0)
+		digit_len = 0;
+	if ((context -> spec -> flags & FLAG_PREC) && \
+(size_t)context -> spec -> precision > digit_len)
+		paper.prec_zeros = context -> spec -> precision - digit_len;
+	paper.core_len = digit_len + paper.prec_zeros;
 	paper.core = start;
-	printer_len = ft_printf_print_config(context, &paper);
-	return (printer_len);
+	return (ft_printf_print_config(context, &paper));
 }
 //sign is supposed to be handled by ft_print_sign_handler
 
@@ -43,26 +49,21 @@ static void	print_sign(const t_context *context, t_print *paper, int arg)
 		paper -> sign = ' ';
 }
 
-static size_t	itoa_no_sign(int n, char buf[12], const char **start)
+static size_t	itoa_no_sign(unsigned int n, char buf[12], const char **start)
 {
-	int		i;
-	long	n_long;
+	int i = 10;
 
-	i = 10;
-	n_long = (long)n;
-	if (n < 0)
-		n_long = -n_long;
 	buf[11] = '\0';
-	if (n_long == 0)
+	if (n == 0)
 	{
 		buf[i] = '0';
 		*start = &buf[i];
 		return (1);
 	}
-	while (n_long != 0)
+	while (n != 0)
 	{
-		buf[i] = '0' + (n_long % 10);
-		n_long /= 10;
+		buf[i] = '0' + (n % 10);
+		n /= 10;
 		i--;
 	}
 	*start = &buf[i + 1];
