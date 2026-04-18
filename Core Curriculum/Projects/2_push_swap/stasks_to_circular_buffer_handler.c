f7d8f82b5637181cddb10530295b0e32a16d5043
Christopher Hui-Kang Nah <christopher_nah@yahoo.com.sg>
2026-04-18 17:33:18 +0800

    Add circular buffer handler for push_swap project

---
+++ Core Curriculum/Projects/2_push_swap/stasks_to_circular_buffer_handler.c
@@ -0,0 +1,19 @@
+#include "push_swap.h"
+
+int insert_num_to_cbuf(cbuf *stack, int num)
+{
+	
+	return (0);
+}
+
+int get_num_from_cbuf(cbuf *stack, int num)
+{
+	
+	return (0);
+}
+
+int remove_num_from_cbuf(cbuf *stack, int num)
+{
+	
+	return (0);
+}
