052acf74b051cc239ffb11cacb2757ead9c66a51
hnah 42SG <hnah@student.42singapore.sg>
2026-05-05 18:34:17 +0800

    StudyNotesForAlgoBook+push_swap checker 20260505:1834

--- Core Curriculum/Projects/2_push_swap/parse_sort_input.c
+++ Core Curriculum/Projects/2_push_swap/parse_sort_input.c
@@ -4,32 +4,5 @@ int parse_input(input *input, char **argv)
 {
 	input->input_len = 
 	input->sorted_input = malloc(sizeof(int) * (size + 1));
-	if (!stack->buffer)
-		return (ERROR);
-	stack->capacity = size;
-	stack->read_idx = 0;
-	stack->write_idx = 0;
-	return (SUCCESS);
-}
-
-int	cbuf_len(cbuf *stack)
-{
-	return ((stack->write_idx - stack->read_idx + stack->capacity) \
-% stack->capacity);
-}
-
-int	cbuf_is_empty(cbuf *stack)
-{
-	return (stack->read_idx == stack->write_idx);
-}
-
-int	cbuf_is_full(cbuf *stack)
-{
-	return ((stack->write_idx + 1) % stack->capacity == stack->read_idx);
-}
-
-void	cbuf_free(cbuf *stack)
-{
-	free(stack->buffer);
-	stack->buffer = NULL;
+	return(SUCCESS);
 }
