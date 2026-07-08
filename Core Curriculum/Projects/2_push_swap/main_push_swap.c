62d8ae13400a9cd5838c2586ebccf64dd4edd119
hnah 42SG <hnah@student.42singapore.sg>
2026-07-08 18:39:47 +0800

    EODJul8/PassExam

--- Core Curriculum/Projects/2_push_swap/main_push_swap.c
+++ Core Curriculum/Projects/2_push_swap/main_push_swap.c
@@ -6,23 +6,38 @@
 /*   By: hnah <hnah@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 13:41:55 by hnah              #+#    #+#             */
-/*   Updated: 2026/07/03 08:44:36 by hnah             ###   ########.fr       */
+/*   Updated: 2026/07/08 18:15:16 by hnah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int count_int_in_str(char *str)
+{
+	int count = 0;
+	int i = 0;
+
+	while (str[i])
+	{
+		if (ft_isdigit(str[i]))
+		{
+			count++;
+			while (ft_isdigit(str[i]))
+				i++;
+		}
+		i++;
+	}
+	return (count);
+}
+
 int	main(int argc, char **argv)
 {
-	int	*yoink_number = NULL;
-	int	temp_error_clean_up_placeholder = 0;
-	if (argc != 2 || argv == temp_error_clean_up_placeholder)
+	int count = 0;
+	
+	if (argc < 2)
 		return (ERR_INVALID_INPUT);
-	else
-	{
-		if (parse_input(argv) == ERROR)
-			return (ERR_PARSE_INPUT);
-		
-	}
+	while(argc-- > 1)
+		count += count_int_in_str(argv[argc]);
+	printf("%i\n", count);
 	return (SUCCESS);
 }
