76e1e71bc534a1c6adf0874102580638b212f058
Christopher Hui-Kang Nah <christopher_nah@yahoo.com.sg>
2026-07-22 18:43:12 +0800

    Update input validation and debugging output in push_swap.

--- Core Curriculum/Projects/2_push_swap/parse_sort_input.c
+++ Core Curriculum/Projects/2_push_swap/parse_sort_input.c
@@ -10,22 +10,20 @@
 int count_int_in_str(const char *str, int *count, int *values)
 {
 	char	*str_moving;
+	int		pos;
 	
+	pos = 1;
 	str_moving = (char *)str;
 	if ((*str_moving && ft_isdigit(*str_moving)) || *str_moving == '+' || *str_moving == '-')
 	{
 		values[*count] = ft_atoi(str_moving);
 		if (check_duplicate(count, values) == ERROR)
 			return (ERROR);
+		if (*str_moving == '-')
+			pos = 0;	
 		while (*str_moving == '+' || *str_moving == '-')
 			str_moving++;
-		while (*str_moving && ft_isdigit(*str_moving))
-			str_moving++;	
-		if (!*str_moving)
-		{
-			(*count)++;
-			return (SUCCESS);
-		}
+		return (check_if_proper_int(str_moving, pos));
 	}
 	return (ERROR);
 }
@@ -48,4 +46,27 @@ int	check_duplicate(const int *count, const int *values)
 		cur_compare--;
 	}
 	return (SUCCESS);
-}
+	
+int	check_if_proper_int(char *str_moving, int pos)
+{
+	int	num_digits;
+	
+	num_digits = 1;
+	while (*str_moving && ft_isdigit(*str_moving))
+	{
+		if (num_digits > 10)
+			break;
+		else if (num_digits == 9 && *str_moving > 4)
+			return (ERROR);
+		else if (!pos && *str_moving > 7) || *str_moving > 8)
+			return (ERROR);
+		str_moving++;
+		num_digits++;
+	}
+	if (!*str_moving)
+	{
+		(*count)++;
+		return (SUCCESS);
+	}
+	return (ERROR);	
+}	
