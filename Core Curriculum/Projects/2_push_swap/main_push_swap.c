fc6d93038fb34644e9700e60b9b958e9d01d8b7a
Christopher Hui-Kang Nah <christopher_nah@yahoo.com.sg>
2026-07-22 19:43:35 +0800

    Fix formatting and add debug print statement
    - Adjusted spacing in condition check
    - Added debug output for value and count

--- Core Curriculum/Projects/2_push_swap/main_push_swap.c
+++ Core Curriculum/Projects/2_push_swap/main_push_swap.c
@@ -31,7 +31,7 @@ int	main(int argc, char **argv)
 		return (ERR_INVALID_INPUT);
 	while(count + 1 < argc)
 	{
-		if(count_int_in_str(argv[count + 1], &count, values) == ERROR)
+		if (count_int_in_str(argv[count + 1], &count, values) == ERROR)
 			return (free_and_error());
 	}
 	return (SUCCESS);
