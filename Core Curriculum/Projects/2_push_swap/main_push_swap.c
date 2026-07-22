6d22231789f7c74cc4ce329a4a3f87303b2925ae
Christopher Hui-Kang Nah <christopher_nah@yahoo.com.sg>
2026-07-22 14:26:47 +0800

    Fix infinite loop in count_int_in_str function

--- Core Curriculum/Projects/2_push_swap/main_push_swap.c
+++ Core Curriculum/Projects/2_push_swap/main_push_swap.c
@@ -27,7 +27,7 @@ int count_int_in_str(const char *str)
 		{
 			count++;
 			values[count-1] = ft_atoi(str_moving);
-			while (ft_isdigit(*str_moving));
+			while (ft_isdigit(*str_moving))
 				str_moving++;
 		}
 	}
