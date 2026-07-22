1858d81a686ed991929dccc29ea8cb46b123f90f
Christopher Hui-Kang Nah <christopher_nah@yahoo.com.sg>
2026-07-22 17:02:07 +0800

    Refactor count_int_in_str and add duplicate check

--- Core Curriculum/Projects/2_push_swap/main_push_swap.c
+++ Core Curriculum/Projects/2_push_swap/main_push_swap.c
@@ -12,14 +12,16 @@
 
 #include "push_swap.h"
 
-int count_int_in_str(const char *str, int *count, int **values)
+int count_int_in_str(const char *str, int *count, int *values)
 {
 	char	*str_moving;
 	
 	str_moving = (char *)str;
 	if ((*str_moving && ft_isdigit(*str_moving)) || *str_moving == '+' || *str_moving == '-')
 	{
-		*value[count] = ft_atoi(str_moving);
+		*values[*count] = ft_atoi(str_moving);
+		if (check_duplicate(values) == ERROR)
+			return (ERROR);
 		while (*str_moving == '+' || *str_moving == '-')
 			str_moving++;
 		while (*str_moving && ft_isdigit(*str_moving))
@@ -33,11 +35,32 @@ int count_int_in_str(const char *str, int *count, int **values)
 	return (ERROR);
 }
 
+int	check_duplicate(const int *count, const int *values)
+{
+	int cur_compare;
+	int looping_index;
+	
+	cur_compare = looping_count = count;
+	looping_index = cur_compare - 1;
+	while (cur_compare > 0)
+	{
+		looping_index = cur_compare - 1;
+		while (looping_index >= 0)
+		{
+			if (values[looping_index] == values[cur_compare])
+				return (ERROR);
+		}
+		cur_compare--;
+	}
+	return (SUCCESS);
+}
+
 int free_and_error()
 {
 	ft_putendl_fd("Error", 2);
 	return (ERROR);
 }
+
 
 int	main(int argc, char **argv)
 {
@@ -49,7 +72,7 @@ int	main(int argc, char **argv)
 		return (ERR_INVALID_INPUT);
 	while(count + 1 < argc)
 	{
-		if(count_int_in_str(argv[count + 1], &count, values) == ERROR)
+		if(count_int_in_str(argv[count + 1], &count, &values[count]) == ERROR)
 			return (free_and_error());
 		printf("Cur num: %i\n", values[count-1]);
 	}
