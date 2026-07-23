d529f41292da95ce7a80d42aeb111625e2643e59
Christopher Hui-Kang Nah <christopher_nah@yahoo.com.sg>
2026-07-23 12:59:10 +0800

    Fix buffer access in push and pop functions

--- Core Curriculum/Projects/2_push_swap/circular_buffer_insert_remove.c
+++ Core Curriculum/Projects/2_push_swap/circular_buffer_insert_remove.c
@@ -20,7 +20,7 @@ int	cbuf_push_top(cbuf *stack, int number)
 	if (cbuf_is_full(stack))
 		return (ERROR);
 	stack->read_idx = (stack->read_idx - 1 + cbuf_cap) % cbuf_cap;
-	stack->buffer[stack->read_idx] = number;
+	stack->buf[stack->read_idx] = number;
 	return (SUCCESS);
 }
 
@@ -31,7 +31,7 @@ int	cbuf_push_bottom(cbuf *stack, int number)
 	cbuf_cap = stack->capacity;
 	if (cbuf_is_full(stack))
 		return (ERROR);
-	stack->buffer[stack->write_idx] = number;
+	stack->buf[stack->write_idx] = number;
 	stack->write_idx = (stack->write_idx + 1) % cbuf_cap;
 	return (SUCCESS);
 }
@@ -43,7 +43,7 @@ int	cbuf_pop_top(cbuf *stack, int *pop_number)
 	cbuf_cap = stack->capacity;
 	if (cbuf_is_empty(stack))
 		return (ERROR);
-	*pop_number = stack->buffer[stack->read_idx];
+	*pop_number = stack->buf[stack->read_idx];
 	stack->read_idx = (stack->read_idx + 1) % cbuf_cap;
 	return (SUCCESS);
 }
@@ -56,7 +56,7 @@ int	cbuf_pop_bottom(cbuf *stack, int *pop_number)
 	if (cbuf_is_empty(stack))
 		return (ERROR);
 	stack->write_idx = (stack->write_idx - 1 + cbuf_cap) % cbuf_cap;
-	*pop_number = stack->buffer[stack->write_idx];
+	*pop_number = stack->buf[stack->write_idx];
 
 	return (SUCCESS);
 }
