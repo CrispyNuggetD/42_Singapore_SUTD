d762627c57b15e64f2a081d7c8015ffee901def2
Christopher Hui-Kang Nah <christopher_nah@yahoo.com.sg>
2026-07-23 12:52:43 +0800

    Add cbuf_print and metadata printing functions

--- Core Curriculum/Projects/2_push_swap/debug_tools.c
+++ Core Curriculum/Projects/2_push_swap/debug_tools.c
@@ -15,3 +15,43 @@ void	debug_print_int_array(const int *array, int size)
 	}
 	printf("\n");
 }
+
+static void	cbuf_print_metadata(cbuf *stack, char name)
+{
+	ft_putchar_fd(name, 2);
+	ft_putstr_fd(" [len=", 2);
+	ft_putnbr_fd(cbuf_len(stack), 2);
+	ft_putstr_fd(" read=", 2);
+	ft_putnbr_fd(stack->read_idx, 2);
+	ft_putstr_fd(" write=", 2);
+	ft_putnbr_fd(stack->write_idx, 2);
+	ft_putstr_fd(" capacity=", 2);
+	ft_putnbr_fd(stack->capacity, 2);
+	ft_putstr_fd("]: ", 2);
+}
+
+void	cbuf_print(cbuf *stack, char name)
+{
+	int	offset;
+	int	index;
+	int	length;
+
+	length = cbuf_len(stack);
+	cbuf_print_metadata(stack, name);
+	offset = 0;
+	while (offset < length)
+	{
+		index = (stack->read_idx + offset) % stack->capacity;
+		ft_putnbr_fd(stack->buffer[index], 2);
+		if (offset + 1 < length)
+			ft_putchar_fd(' ', 2);
+		offset++;
+	}
+	ft_putchar_fd('\n', 2);
+}
+
+void	cbuf_print_stacks(cbuf *a, cbuf *b)
+{
+	cbuf_print(a, 'A');
+	cbuf_print(b, 'B');
+}
