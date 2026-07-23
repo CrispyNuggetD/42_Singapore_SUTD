5cfbe819ecf36a893c089bc1949f70722b1d0290
Christopher Hui-Kang Nah <christopher_nah@yahoo.com.sg>
2026-07-23 14:36:30 +0800

    Add bottom_idx variable to cbuf_rev_rotate function

--- Core Curriculum/Projects/2_push_swap/circular_buffer_move_element.c
+++ Core Curriculum/Projects/2_push_swap/circular_buffer_move_element.c
@@ -32,15 +32,17 @@ int	cbuf_rotate(cbuf *stack)
 int	cbuf_rev_rotate(cbuf *stack)
 {
 	int	top_idx;
+	int	bottom_idx;
 	int	cbuf_cap;
 
 	if (cbuf_len(stack) < 2)
 		return (SUCCESS);
 	top_idx = stack->read_idx;
 	cbuf_cap = stack->capacity;
-	stack->write_idx = (stack->write_idx - 1 + cbuf_cap) % cbuf_cap;
-	stack->buf[top_idx] = stack->buf[write_idx];
+	bottom_idx = (stack->write_idx - 1 + cbuf_cap) % cbuf_cap;
+	stack->buf[top_idx] = stack->buf[bottom_idx];
 	stack->read_idx = (stack->read_idx - 1 + cbuf_cap) % cbuf_cap;
+	stack->write_idx = bottom_idx
 	return (SUCCESS);
 }
 
