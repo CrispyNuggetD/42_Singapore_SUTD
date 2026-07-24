fac72fe696bf23c5c7b3eac9093bf2e4b543f692
Christopher Hui-Kang Nah <christopher_nah@yahoo.com.sg>
2026-07-24 11:56:16 +0800

    Refactor error handling in main_push_swap.c
    - Updated error handling to use free_and_error() function.
    - Replaced direct ERROR returns with function calls.

--- Core Curriculum/Projects/2_push_swap/main_push_swap.c
+++ Core Curriculum/Projects/2_push_swap/main_push_swap.c
@@ -13,13 +13,11 @@
 #include "push_swap.h"
 //printf("Cur num: %i\nCur index: %i\n", values[count-1], count-1);
 
-/*
 int	free_and_error(void)
 {
-	ft_putendl_fd("Error", 2);
+	ft_putendl_fd("Error", STDERR_FILENO);
 	return (ERROR);
 }
-*/
 
 int	main(int argc, char **argv)
 {
@@ -36,19 +34,19 @@ int	main(int argc, char **argv)
 	while (i < argc)
 	{
 		if (count_int_in_str(argv[i++], &count, b.buf) == ERROR)
-			return (ERROR);
+			return (free_and_error());
 	}
 	if (count < 2)
-		return (ERROR);
+		return (free_and_error());
 	if (rank_values(count, b.buf, a.buf) == ERROR)
-		return (ERROR);
+		return (free_and_error());
 	cbuf_info(&a, &b, count);
 	if (soln_init(&x, 10, 10000) == ERROR)
-		return (ERROR);
+		return (free_and_error());
 	if (solve(&x, &a, &b) == ERROR)
-		return (ERROR);
+		return (free_and_error());
 	if (print_first_soln(&x) == ERROR)
-		return (ERROR);
+		return (free_and_error());
 	/*
 	printf("\n values: \n");
 	debug_print_int_array(b.buf, count);
